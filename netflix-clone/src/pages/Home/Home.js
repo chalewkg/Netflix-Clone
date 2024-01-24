3a80a15e06548e4e879b0b731862d004bf3db33c
chalewkg <chalewkg@gmail.com>
2024-01-23 22:57:04 -0500

    banner created

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header.js";
 import Footer from "../../components/Footer/Footer.js";
+import Banner from "../../components/Banner/Banner.js";
 
 const Home = () => {
     return (<>
         <Header />
+        <Banner />
         <Footer />
     </>
     )
